01c07ab6578e78b4c61eb05a9779cedd38e445a4
Kolpa <kolyaopahle@gmail.com>
2013-01-05 17:54:01 +0100

    keyconfig v2

--- FF4Bot/Keys.cs
+++ FF4Bot/Keys.cs
@@ -11,6 +11,17 @@ namespace FF4Bot
        public static readonly Dictionary<Int32, VirtualKeyCode> vb2vk()
        {
            Dictionary<Int32, VirtualKeyCode> tmp = new Dictionary<int, VirtualKeyCode>();
+           tmp.Add(16, VirtualKeyCode.VK_Q);
+           tmp.Add(17, VirtualKeyCode.VK_W);
+           tmp.Add(18, VirtualKeyCode.VK_E);
+           tmp.Add(19, VirtualKeyCode.VK_R);
+           tmp.Add(20, VirtualKeyCode.VK_T);
+           tmp.Add(21, VirtualKeyCode.VK_Z);
+           tmp.Add(22, VirtualKeyCode.VK_U);
+           tmp.Add(23, VirtualKeyCode.VK_I);
+           tmp.Add(24, VirtualKeyCode.VK_O);
+           tmp.Add(25, VirtualKeyCode.VK_P);
+
            tmp.Add(30, VirtualKeyCode.VK_A);
            tmp.Add(31, VirtualKeyCode.VK_S);
            tmp.Add(32, VirtualKeyCode.VK_D);
@@ -20,28 +31,14 @@ namespace FF4Bot
            tmp.Add(36, VirtualKeyCode.VK_J);
            tmp.Add(37, VirtualKeyCode.VK_K);
            tmp.Add(38, VirtualKeyCode.VK_L);
-           tmp.Add(16, VirtualKeyCode.VK_Q);
-           tmp.Add(17, VirtualKeyCode.VK_W);
-           tmp.Add(18, VirtualKeyCode.VK_E);
-           tmp.Add(19, VirtualKeyCode.VK_R);
-           return tmp;
-       }
-       public static readonly Dictionary<String, VirtualKeyCode> vk2dir()
-       {
-           Dictionary<String, VirtualKeyCode> tmp = new Dictionary<String, VirtualKeyCode>();
-           tmp.Add("Up", VirtualKeyCode.VK_A);
-           tmp.Add("Down", VirtualKeyCode.VK_S);
-           tmp.Add("Left", VirtualKeyCode.VK_D);
-           tmp.Add("Right", VirtualKeyCode.VK_F);
-           tmp.Add("A", VirtualKeyCode.VK_G);
-           tmp.Add("B", VirtualKeyCode.VK_H);
-           tmp.Add("L", VirtualKeyCode.VK_J);
-           tmp.Add("R", VirtualKeyCode.VK_K);
-           tmp.Add("Select", VirtualKeyCode.VK_L);
-           tmp.Add("Start", VirtualKeyCode.VK_Q);
-           tmp.Add("Speed", VirtualKeyCode.VK_W);
-           tmp.Add("Capture", VirtualKeyCode.VK_E);
-           tmp.Add("GS", VirtualKeyCode.VK_R);
+
+           tmp.Add(44, VirtualKeyCode.VK_Y);
+           tmp.Add(45, VirtualKeyCode.VK_X);
+           tmp.Add(46, VirtualKeyCode.VK_C);
+           tmp.Add(47, VirtualKeyCode.VK_V);
+           tmp.Add(48, VirtualKeyCode.VK_B);
+           tmp.Add(49, VirtualKeyCode.VK_N);
+           tmp.Add(50, VirtualKeyCode.VK_M);
            return tmp;
        }
     }
